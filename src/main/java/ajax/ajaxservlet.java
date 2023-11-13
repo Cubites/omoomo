1a0dc0d38942b83224abeb0e9430fb1f82177561
mimi5963 <mimi5963@naver.com>
2023-11-13 17:24:42 +0900

    jangwon Ajax And SingleTon

--- src/main/java/ajax/ajaxservlet.java
+++ src/main/java/ajax/ajaxservlet.java
@@ -23,16 +23,20 @@ public class ajaxservlet extends HttpServlet {
    	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
+		
+		//파라미터로 이름을 받아서 
 		String name = request.getParameter("name");
 		
 		System.out.println(name + "login");
 		
 		UserDAO dao = new UserDAO();
+		
+		//LIKE에 들어갈 이름을 매개변수로 넣어서 list받아옴 
 		List<UserVO> list = dao.MemberList(name);
 		
 		JsonArray jsonArray = new JsonArray();
 		
-		
+		//list에서 VO를 꺼내서 JSON Object로 만들기 
 		for(UserVO vo : list) {
 			JsonObject jo = new JsonObject();
 			jo.addProperty("userName", vo.getUser_name());
@@ -41,10 +45,13 @@ public class ajaxservlet extends HttpServlet {
 			jo.addProperty("rank", vo.getRanking());
 			jsonArray.add(jo);
 		}
+		
+		//json을 보내기 위해 String으로 변환
 		String jsonString = jsonArray.toString();
 		
 		System.out.println(jsonString);
 		
+		//!! HTTP MessageBody에 값을 넣기 위해서는 반드시 ContentType을 지정해야함 
 		response.setContentType("application/json");
 		response.setCharacterEncoding("utf-8");
 		
--- src/main/java/room/RoomMap.java
+++ src/main/java/room/RoomMap.java
@@ -5,19 +5,27 @@ import java.util.Map;
 import java.util.Set;
 
 public class RoomMap {
+    // Room정보를 저장하기 위한 map을 wrap하는 싱글톤 RoomMap객체  기능은 map과 동일함
+    // ** 자바에서 컬렉션을 사용할 때 이와 같이 경계클래스(RoomMap)같은 것을 두고, wrapping을 종종 한다
+    // 그 이유는 1. 기본 Map컬렉션이 제공하는 기능이 너무 많음으로, 필요한 기능만 정의해두고 사용하면, 편하다.
+    //        2. 컬렉션 객체를 불변 객체로 사용하기 위해서 필요하다. (방어적 복사)
+   
     private Map<String, RoomVO> map;
-
+    
+    //객체 생성없이 클래스 레벨에서 변수를 지정하고 사용해야함 static으로 선언 
     private static RoomMap roomMap;
-
+    
+    //객체 생성 못하도록 public으로 만들기
     private RoomMap() {
         map = new HashMap<>();
     }
 
+    //미리 생성된 객체의 인스턴스를 얻기 위해서 사용하는 메서드 
     public static RoomMap getRoomMap() {
-        if (roomMap == null) {
-            roomMap = new RoomMap();
+        if (roomMap == null) { //생성된 싱글톤 객체가 없다면,
+            roomMap = new RoomMap(); //싱글톤 객체 생성
         }
-        return roomMap;
+        return roomMap; //roomMap return 
     }
 
     public RoomVO put(String key, RoomVO vo) {
@@ -39,7 +47,8 @@ public class RoomMap {
     public RoomVO get(String key) {
         return map.get(key);
     }
-
+    
+    //테스트 용도 
     public String RoomName() {
         System.out.println("여기왔다.");
         String roomName = "";
